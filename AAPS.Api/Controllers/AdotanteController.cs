983853d789699c7ef0619bc9acc1ea80ab406ccc
alissonnL <111125675+alissonnL@users.noreply.github.com>
2024-11-08 18:55:41 -0300

    Add files via upload
    Adição de endpoint Adotante

---
+++ AAPS.Api/Controllers/AdotanteController.cs
@@ -0,0 +1,59 @@
+﻿using AAPS.Api.Dtos;
+using AAPS.Api.Models;
+using AAPS.Api.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AAPS.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+public class AdotanteController : Controller
+{
+    private readonly IAdotanteService _adotanteService;
+
+    public AdotanteController(IAdotanteService adotanteService)
+    {
+        _adotanteService = adotanteService;
+    }
+
+    [HttpGet]
+    [Route("ObterTodosAdotantes")]
+    public async Task<ActionResult<IAsyncEnumerable<Adotante>>> ObterAdotantes()
+    {
+        var adotantesDto = await _adotanteService.ObterAdotantes();
+        return Ok(adotantesDto);
+    }
+
+    [HttpGet]
+    [Route("ObterAdotantesPorNome")]
+    public async Task<ActionResult<IAsyncEnumerable<Adotante>>> ObterAdotantePorNome(string nome)
+    {
+        var adotantes = await _adotanteService.ObterAdotantePorNome(nome);
+        return Ok(adotantes);
+    }
+
+    [HttpPost]
+    [Route("Criar")]
+    public async Task CriarAdotante(AdotanteDto adotanteDto)
+    {
+        await _adotanteService.CriarAdotante(adotanteDto);
+    }
+
+    [HttpPut]
+    [Route("Atualizar/{id:int}")]
+    public async Task AtualizarAdotante(int id, AdotanteDto adotanteDto)
+    {
+        await _adotanteService.AtualizarAdotante(id, adotanteDto);
+    }
+
+    [HttpDelete]
+    [Route("Delete/{id:int}")]
+    public async Task<ActionResult> ExcluirAdotante(int id)
+    {
+        await _adotanteService.ExcluirAdotante(id);
+        return Ok($"Adotante de id = {id} foi excluído com sucesso!");
+    }
+}
