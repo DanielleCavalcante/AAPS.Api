c29271eb19212630247ecfa8ae47db4b90d75ad6
DanielleCavalcante <daniellesoucavalcante1324@gmail.com>
2025-06-08 14:37:01 -0300

    ajuste no relatorio para poder filtra pelo tipo de relatório

---
+++ AAPS.Api/Models/Enums/TipoRelatorio.cs
@@ -0,0 +1,10 @@
+﻿namespace AAPS.Api.Models.Enums
+{
+    public enum TipoRelatorio
+    {
+        Geral = 0,
+        PorEspecie = 1,
+        BalancoMensal = 2,
+        LevantamentoAnimais = 3
+    }
+}
